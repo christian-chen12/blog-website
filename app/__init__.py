e22b40f81d685e044be08bb453ed56c4ab21c388
Cooper Chen <cooper@Coopers-MacBook-Pro-2.local>
2021-02-18 23:35:06 -0500

    bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 # initiate the Flask app
@@ -27,6 +28,9 @@ login.login_view = 'login'
 
 # uses flask_mail to send emails
 mail = Mail(app)
+
+# uses flask-bootstrap to style html
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
